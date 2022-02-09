f8a64a4af787cd2cb31b377d0649cd20f982a61d
Thomas Boyle <tpboyle2@gmail.com>
2022-02-09 08:41:19 +0200

    updates
    finished lesson 31, onto 32: https://www.udemy.com/course/minecraft-modding-fabric-118/learn/lecture/29614414#content

--- src/main/java/net/lttomboyle/mccourse/MCCourseMod.java
+++ src/main/java/net/lttomboyle/mccourse/MCCourseMod.java
@@ -1,17 +1,19 @@
 package net.lttomboyle.mccourse;
 
 import net.fabricmc.api.ModInitializer;
+import net.lttomboyle.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCCourseMod implements ModInitializer {
-
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/lttomboyle/mccourse/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.lttomboyle.mccourse.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.lttomboyle.mccourse.MCCourseMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+    public static final Item ORICHALCUM_INGOT = registerItem("orichalcum_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item ORICHALCUM_NUGGET = registerItem("orichalcum_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod items for " + MCCourseMod.MOD_ID);
+    }
+}
