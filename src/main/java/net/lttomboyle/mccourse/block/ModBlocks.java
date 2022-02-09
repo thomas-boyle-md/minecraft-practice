d31eebbcbebe94103965ebbe7c20ef2d21e072b0
Thomas Boyle <tpboyle2@gmail.com>
2022-02-09 14:40:15 +0200

    YES

--- src/main/java/net/lttomboyle/mccourse/block/ModBlocks.java
+++ src/main/java/net/lttomboyle/mccourse/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.lttomboyle.mccourse.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.lttomboyle.mccourse.MCCourseMod;
+import net.lttomboyle.mccourse.item.ModItemGroups;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
@@ -14,7 +15,16 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block ORICHALCUM_BLOCK = registerBlock("orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ItemGroup.MISC);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroups.COURSE);
+
+    public static final Block ORICHALCUM_ORE = registerBlock("orichalcum_ore",
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroups.COURSE);
+
+    public static final Block DEEPLSLATE_ORICHALCUM_ORE = registerBlock("deepslate_orichalcum_ore",
+            new Block(FabricBlockSettings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroups.COURSE);
+
+    public static final Block RAW_ORICHALCUM_BLOCK = registerBlock("raw_orichalcum_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroups.COURSE);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
--- src/main/java/net/lttomboyle/mccourse/item/ModItems.java
+++ src/main/java/net/lttomboyle/mccourse/item/ModItems.java
@@ -3,17 +3,19 @@ package net.lttomboyle.mccourse.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.lttomboyle.mccourse.MCCourseMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 
 public class ModItems {
     public static final Item ORICHALCUM_INGOT = registerItem("orichalcum_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
 
     public static final Item ORICHALCUM_NUGGET = registerItem("orichalcum_nugget",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item RAW_ORICHALCUM = registerItem("raw_orichalcum",
+            new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
 
 
     private static Item registerItem(String name, Item item) {
