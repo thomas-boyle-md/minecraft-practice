b39b8b6282df273394e95a454a73cd890383ac91
Thomas Boyle <tpboyle2@gmail.com>
2022-02-14 16:14:02 +0200

    update
    advanced item, dousing rod

--- src/main/java/net/lttomboyle/mccourse/item/ModItems.java
+++ src/main/java/net/lttomboyle/mccourse/item/ModItems.java
@@ -2,6 +2,7 @@ package net.lttomboyle.mccourse.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.lttomboyle.mccourse.MCCourseMod;
+import net.lttomboyle.mccourse.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -17,6 +18,8 @@ public class ModItems {
     public static final Item RAW_ORICHALCUM = registerItem("raw_orichalcum",
             new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
 
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
